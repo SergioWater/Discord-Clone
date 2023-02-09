0d7082e4a85d38e7e80399932db26981930205ad
Sergio <sergiof_2001@hotmail.com>
2023-02-08 23:55:33 -0800

    added the appSlice

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -5,23 +5,22 @@ import {createSlice } from '@reduxjs/toolkit';
 export const appSlice = createSlice({
   name: 'app',
   initialState:{
-    app: null,
+    channelId: null,
+    channelName: null,
   },
   reducers: {
-    login: (state, action) => {
+    setchannelId: (state, action) => {
         state.app += action.payload;
     },
-    logout: (state) =>{
-        state.app = null;
-    }
   },
 
 });
 
-export const {login, logout} = appSlice.actions;
+export const {setchannelId} = appSlice.actions;
 
 
-export const selectapp = (state) => state.app.app;
+export const selectchannelId = (state) => state.app.channelId;
+export const selectchannelName = (state) => state.app.channelName;
 
 
 export default appSlice.reducer;
