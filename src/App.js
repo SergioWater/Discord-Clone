692a862e11c6490f20c76fd8d50f1b34d8506456
Sergio <sergiof_2001@hotmail.com>
2023-02-09 22:40:03 -0800

    saving

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import {selectUser } from './features/userSlice'
+import Login from './Login';
+
 
 function App() {
+  const user = useSelector(selectUser);
+
+
   return (
     //BEM naming convention
+
     <div className="app">
+      
+      
+      {user ? (
+        <>
+       <Sidebar />
 
-        {/* SideBar */}
-        <Sidebar />
+       <Chat />
+       </>
+      ): <Login/>
+      
+      }
 
-        {/* Chat */}
-        <Chat />
+ 
         
     </div>
+
   );
 }
 
